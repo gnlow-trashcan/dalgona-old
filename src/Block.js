50e8a16c02da6270b70aa7f2f111d1dbe7534577
Gnlow <gnlow@protonmail.com>
2023-10-22 21:14:50 +0900

    impl: Block.moveToRoot

--- src/Block.js
+++ src/Block.js
@@ -1,12 +1,43 @@
-import { makeDraggable } from "./makeDraggable.js"
+const root = document.querySelector("playground")
 
 export class Block extends HTMLElement {
     constructor() {
         super()
         const template = document.getElementById("dalgona-block")
         const content = template.content.cloneNode(true).querySelector("svg")
-        makeDraggable(content)
+        this.makeDraggable(content)
         this.attachShadow({ mode: "open" })
             .appendChild(content)
+    }
+
+    moveToRoot() {
+        this.parentElement.removeChild(this)
+        root.appendChild(this)
+        console.log(this)
+    }
+
+    makeDraggable(target) {
+        target.addEventListener("pointerdown", e => {
+            this.moveToRoot()
+            e.stopPropagation()
+            const {
+                e: initX,
+                f: initY,
+            } = target.transform.baseVal.getItem(0).matrix
+            const start = {
+                x: e.clientX - initX,
+                y: e.clientY - initY,
+            }
+            const onMove = e => {
+                target.transform.baseVal.getItem(0).setTranslate(
+                    e.clientX - start.x,
+                    e.clientY - start.y,
+                )
+            }
+            document.body.addEventListener("pointermove", onMove)
+            document.body.addEventListener("pointerup", e => {
+                document.body.removeEventListener("pointermove", onMove)
+            }, { once: true })
+        })
     }
 }
